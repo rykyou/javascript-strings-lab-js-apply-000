65d4715156848c0f1865345c8ea19f2f2ccd6b56
Robin Kim <rkim1214@gmail.com>
2018-09-19 18:04:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "Let's talk about space.";
 
-var conversation = `${topic}`;
-
-greetSpecialGuest;
-conversation;
+var conversation = `Let's talk about ${topic}.`;
